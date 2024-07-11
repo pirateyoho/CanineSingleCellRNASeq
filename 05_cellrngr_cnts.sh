2ca3814708ae36d721199dd070f70720a49fa0a3
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2024-07-11 13:28:30 -0600

    Update 05_cellrngr_cnts.sh

--- 05_cellrngr_cnts.sh
+++ 05_cellrngr_cnts.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # Purpose: To run cellranger count on single-cell RNAseq fastq files
-###### IMPORTANT: ***sbatchLoop_cellrngr_cnts.sh*** MUST be run *FIRST* as this script is designed to receive input from that script ######
+###### IMPORTANT: ***sbatchLoop_cellrngr_cnts.sh*** MUST be run *FIRST* as this script is designed to receive input from that script. If you want to run this script on only one sample, then execute it in the command line with 'sbatch 05_cellrngr_cnts.sh SampleID', replacing "SampleID" with the fastq filename prefix (all characters before the "_S") ######
 ###### This script should exist in the 02_scripts directory ######
 
 #SBATCH --account=csu84_alpine1
 #SBATCH --job-name=cellrngr_cnt
-#SBATCH --ntasks=24       # modify this number to reflect how many cores you want to use (up to 64)
 #SBATCH --nodes=1         # this script is designed to run on one node
-#SBATCH --time=6:00:00   # set time; default = 4 hours
+#SBATCH --ntasks=64       # modify this number to reflect how many cores you want to use (up to 64)
+#SBATCH --mem=200GB
+#SBATCH --time=24:00:00   # set time; default = 4 hours
 #SBATCH --partition=amilan  # modify this to reflect which queue you want to use. Either 'amilan' or 'atesting'
 #SBATCH --qos=normal      # modify this to reflect which queue you want to use. Options are 'normal' and 'testing'
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT   # Keep these two lines of code if you want an e-mail sent to you when it is complete.
